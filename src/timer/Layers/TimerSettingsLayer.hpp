7818c1fbd8c5fad740f8e59e2d37ac63a8cc1a66
Hbg1010 <Harrisong26@parkschool.net>
2024-11-24 22:42:46 -0500

    made button functions have propper requests + variables

--- src/timer/Layers/TimerSettingsLayer.hpp
+++ src/timer/Layers/TimerSettingsLayer.hpp
@@ -7,10 +7,11 @@ class TimerSettingsLayer : public Popup<std::string const&> {
 
     public:
         static TimerSettingsLayer* create(std::string const& menuID);
+        std::string m_menuID;
 
     protected:
         bool setup(std::string const& menuID);
-        void resetTimer();
-        void changeSettings();
-        void addTime();
+        void resetTimer(CCObject* sender);
+        void changeSettings(CCObject* sender);
+        void addTime(CCObject* sender);
 };
