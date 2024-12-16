446ec274a917d8b039b224d389737a6c17f49fee
hbg1010 <harrisong26@parkschool.net>
2024-12-15 20:05:58 -0500

    One timer layer may exist at a time now

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,18 @@ $execute{
 		if (ev->isActive()) {
 
 			std::string timerAlert = fmt::format("Time to take a {} second break!", Mod::get()->getSettingValue<int64_t>("breakTime"));
+			CCNode* parent = ev->getCurrentLayer(); 
 
+			while(parent->getParent() != nullptr) {
+				parent = parent->getParent();
+			}
+
+			// should stop c
+			if (parent->getChildByID("TimerAlert"_spr) != nullptr && parent->getChildByID("Timer-pop-up"_spr) != nullptr) {
+				log::debug("layer exists");
+				return ListenerResult::Stop;
+			}
+			
 			// creates popup before the timer
 			auto x = geode::createQuickPopup("Timer", timerAlert, "Start", "Skip", [ev](auto, bool btn2) {
 				
@@ -28,32 +39,25 @@ $execute{
 						CCNode* x = CCNode::create(); 
 						TimerLayer* timePopUp = TimerLayer::create(fmt::format("{}", Mod::get()->getSettingValue<int64_t>("breakTime")));
 
-						// returns the grandfather of a node, so The popup is placed at the top of the tree
-						CCNode* parent = ev->getCurrentLayer(); 
-
-						while(parent->getParent() != nullptr) {
-							parent = parent->getParent();
-						}
 						timePopUp->m_noElasticity = CCDirector::get()->getFastMenu();
-						parent->addChild(timePopUp, 20);
+						timePopUp->show();
 						timePopUp->setID("Timer-pop-up"_spr);
 				} 
 		}, false);
 
 		// displays x
-		x->m_noElasticity = CCDirector::get()->getFastMenu();
+		x->m_noElasticity = CCDirector::get()->getFastMenu(); // stops intro animation when fast reset is used
 		x->show();
+		x->setID("TimerAlert"_spr);
 
 		// if in editor, this will set the touch priority above the EditorPauseLayer
 		// very open to knowing a better way to do this! ! ! Unlike the editorUI though, theres no get function :(
 		auto editorPauseLayer = ev->getCurrentLayer()->getParent()->getChildByID("EditorPauseLayer");
 		if (editorPauseLayer != nullptr) x->setTouchPriority(static_cast<EditorPauseLayer*>(editorPauseLayer)->getTouchPriority() - 1);
 
-		x->setID("TimerAlert"_spr);
-
 			return ListenerResult::Stop;
-
 		}
+
 		return ListenerResult::Propagate;
 	});
 };
