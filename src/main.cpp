9e45f1a07ffca2ab55a043316dce60eeaa4bc7d1
hbg1010 <harrisong26@parkschool.net>
2025-03-24 15:00:44 -0400

    onload to edit jsons, and cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,29 @@
  */
 using namespace geode::prelude;
 
-// this executes the event when it occurs
-$execute{
+#ifdef SHORTEN_TIMER_TIME 
+	#define TMULT 1
+#else
+	#define TMULT 60
+#endif
 
+$on_mod(Loaded) {
+	Mod::get()->setSavedValue<float>("savedTime", Mod::get()->getSettingValue<int>("interval") * TMULT);
+    if (Mod::get()->getSettingValue<bool>("resetPause")) Mod::get()->setSavedValue<bool>("timerPaused", false);
+
+    #ifdef extraPrints
+        log::debug("settings reset!");
+    #endif
+};
+
+$execute{
 	// handles popups on both editor and pause layer
 	new EventListener<EventFilter<TimerEvent>>(+[](TimerEvent* ev) {
 		if (ev->isActive()) {
-
-			// std::string timerAlert = fmt::format("Time to take a {} second break!", Mod::get()->getSettingValue<int64_t>("breakTime"));
+			// resets timer sav val if needed on event
+			if (Mod::get()->getSettingValue<bool>("useSaving")) 
+				Mod::get()->setSavedValue<float>("savedTime", Mod::get()->getSettingValue<int>("interval") * TMULT);
+			
 			CCNode* parent = ev->getCurrentLayer(); 
 
 			while(parent->getParent() != nullptr) {
@@ -26,41 +41,43 @@ $execute{
 
 			// should stop copies being created
 			if (parent->getChildByID("TimerAlert"_spr) != nullptr && parent->getChildByID("Timer-pop-up"_spr) != nullptr) {
-				log::debug("layer exists");
+				log::error("timer layer already exists");
 				return ListenerResult::Stop;
 			}
 
 			// creates popup before the timer
 			auto x = geode::createQuickPopup("Timer",
-				 fmt::format("Time to take a {} second break!", 
-					Mod::get()->getSettingValue<int64_t>("breakTime")), 
-					"Start", "Skip", 
-					[ev](auto, bool btn2) {
+				fmt::format("Time to take a {} second break!", 
+				Mod::get()->getSettingValue<int64_t>("breakTime")), 
+				"Start", "Skip", 
+				[ev](auto, bool btn2) {
 					// yeah I hate that !btn2, but the constructor doesnt allow me to flip the things otherwise!
 					if (!btn2) {
-						// adds a new node to the layer parent
-						CCNode* x = CCNode::create(); 
-						TimerLayer* timePopUp = TimerLayer::create(fmt::format("{}", Mod::get()->getSettingValue<int64_t>("breakTime")));
-
+						TimerLayer* timePopUp = TimerLayer::create(
+							fmt::format("{}", Mod::get()->getSettingValue<int64_t>("breakTime")));
 						timePopUp->m_noElasticity = CCDirector::get()->getFastMenu();
 						timePopUp->show();
 						timePopUp->setID("Timer-pop-up"_spr);
-				} 
-		}, false);
+					} 
+				}, false);		
 
-		// displays x
-		x->m_noElasticity = CCDirector::get()->getFastMenu(); // stops intro animation when fast reset is used
-		x->show();
-		x->setID("TimerAlert"_spr);
+			// displays x
+			x->m_noElasticity = CCDirector::get()->getFastMenu(); // stops intro animation when fast reset is used
+			x->show();
+			x->setID("TimerAlert"_spr);
 
-		// if in editor, this will set the touch priority above the EditorPauseLayer
-		// very open to knowing a better way to do this! ! ! Unlike the editorUI though, theres no get function :(
-		// also set to -2 so that it has a higher priority than the timer pause, solving conflicts!
-		auto editorPauseLayer = ev->getCurrentLayer()->getParent()->getChildByID("EditorPauseLayer");
-		if (editorPauseLayer != nullptr) x->setTouchPriority(static_cast<EditorPauseLayer*>(editorPauseLayer)->getTouchPriority() - 2);
+			/*
+			if in editor, this will set the touch priority above the EditorPauseLayer
+			very open to knowing a better way to do this! ! ! Unlike the editorUI though, theres no get function :(
+			also set to -2 so that it has a higher priority than the timer pause, solving conflicts!
+			*/
+			auto editorPauseLayer = ev->getCurrentLayer()->getParent()->getChildByID("EditorPauseLayer");
+			if (editorPauseLayer != nullptr) x->setTouchPriority(static_cast<EditorPauseLayer*>(editorPauseLayer)->getTouchPriority() - 2);
+
+			// returns that the event ended!
 			return ListenerResult::Stop;
 		}
 
-		return ListenerResult::Propagate;
+			return ListenerResult::Propagate;
 	});
 };
