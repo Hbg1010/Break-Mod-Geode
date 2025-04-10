927fcec828df2f6700d2aee9a237ef60750d2f49
hbg1010 <harrisong26@parkschool.net>
2025-04-10 19:33:01 -0400

    will do later

---
+++ src/timer/Layers/TimerWidget.cpp
@@ -0,0 +1,13 @@
+// #include "TimerWidget.hpp"
+
+// TimerWidget* TimerWidget::Create(char const* fntFile) {
+//     auto temp = new TimerWidget();
+
+//     if (init(fntFile)) {
+//         temp->autoRelease();
+//         return temp;
+//     } else {
+//         CC_SAFE_DELETE(temp);
+//         return nullptr
+//     }
+// }
---
+++ src/timer/Layers/TimerWidget.hpp
@@ -0,0 +1,10 @@
+// #pragma once
+// #include <Geode/Geode.hpp>
+// #include <chrono>
+
+// class TimerWidget : public CCLabelBMFont {
+// public:
+//     static TimerWidget* Create(char const* fntFile = "bigFont.fnt");
+// protected:
+//     bool init(char const* fntFile);
+// }
